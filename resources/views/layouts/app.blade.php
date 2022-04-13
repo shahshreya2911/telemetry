e474b4a755b79caad8da983bb963b322b9a19f7c
nilesh khalash <nileshkhalash@gmail.com>
2022-04-13 15:08:40 +0530

    add logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,12 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                    
+                    <!-- <img alt="Logo" src="{{asset('assets')}}/image/groundhogg-logo-black.svg" /> -->
+                </a>
+                <a href="{{ url('/') }}">
+                    <img alt="Logo" src="{{asset('assets')}}/image/groundhogg-logo-black.svg" width="100px" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
