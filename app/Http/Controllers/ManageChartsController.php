12d1f99051ac2878191f205e9219c18a19275503
nilesh khalash <nileshkhalash@gmail.com>
2022-05-24 12:40:47 +0530

    got all data

--- app/Http/Controllers/ManageChartsController.php
+++ app/Http/Controllers/ManageChartsController.php
@@ -44,7 +44,7 @@ class ManageChartsController extends Controller
         {
             if(!isset($marchCount[$i]))
             {
-                $marchCount[$i] = '';
+                $marchCount[$i] = 0;
             }
         }
         // $marchAllData = ksort($marchCount);
@@ -56,7 +56,7 @@ class ManageChartsController extends Controller
         {
             if(!isset($AprCount[$i]))
             {
-                $AprCount[$i] = '';
+                $AprCount[$i] = 0;
             }
         }
         // $aprAllData = ksort($AprCount);
@@ -67,7 +67,7 @@ class ManageChartsController extends Controller
         $user = ['25','12','44','20','2','37'];
         $user2 = ['28','5','37','25','10','30'];
 
-        $days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]; 
+        $days = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']; 
         $color = ['#ffcccc','#ffffcc','#ccf2ff','#ffd9cc','#e6ccff','#f2ffcc'];
 
         return view('charts.index')->with('year',json_encode($year,JSON_NUMERIC_CHECK))->with('user',json_encode($user,JSON_NUMERIC_CHECK))->with('user2',json_encode($user2,JSON_NUMERIC_CHECK))->with('color',json_encode($color,JSON_NUMERIC_CHECK))->with('days',json_encode($days,JSON_NUMERIC_CHECK))->with('marchAllData',json_encode($marchAllData,JSON_NUMERIC_CHECK))->with('aprAllData',json_encode($aprAllData,JSON_NUMERIC_CHECK));
