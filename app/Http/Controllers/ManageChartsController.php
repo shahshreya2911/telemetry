3eec23348b1dd9fcd3dc86ef761242cdacdf57b5
nilesh khalash <nileshkhalash@gmail.com>
2022-06-21 18:23:01 +0530

    events

--- app/Http/Controllers/ManageChartsController.php
+++ app/Http/Controllers/ManageChartsController.php
@@ -72,24 +72,22 @@ class ManageChartsController extends Controller
         $days = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']; 
         $color = ['#ffcccc','#ffffcc','#ccf2ff','#ffd9cc','#e6ccff','#f2ffcc'];
 
-        echo '<pre>'; 
-        echo '<hr> days  list  : <br>  '; 
-        print_r($days);
-        echo '<hr> APR data   list  : <br>  '; 
-        print_r($aprAllData);
-        echo '<hr> march data  list  : <br>  '; 
-        print_r($marchAllData);
+        // echo '<pre>'; 
+        // echo '<hr> days  list  : <br>  '; 
+        // print_r($days);
+        // echo '<hr> APR data   list  : <br>  '; 
+        // print_r($aprAllData);
+        // echo '<hr> march data  list  : <br>  '; 
+        // print_r($marchAllData);
 
 
-        exit; 
+        // exit; 
 
         return view('charts.index')->with('year',json_encode($year,JSON_NUMERIC_CHECK))->with('user',json_encode($user,JSON_NUMERIC_CHECK))->with('user2',json_encode($user2,JSON_NUMERIC_CHECK))->with('color',json_encode($color,JSON_NUMERIC_CHECK))->with('days',json_encode($days,JSON_NUMERIC_CHECK))->with('marchAllData',json_encode($marchAllData,JSON_NUMERIC_CHECK))->with('aprAllData',json_encode($aprAllData,JSON_NUMERIC_CHECK));
     }
 
 
     public function eventChart(){
-        echo 'here '; 
-        echo '<pre> '; 
         
         $today= Carbon::now();
         $totaldays = $today->month($today->month)->daysInMonth;
@@ -97,10 +95,6 @@ class ManageChartsController extends Controller
         for ($i=1; $i <= $totaldays ; $i++) { 
             array_push($days,$i);
         }
-
-        echo ' | month: '.$today->month;
-        echo ' | year: '.$today->year;
-
 
 
         // $days=cal_days_in_month(CAL_GREGORIAN,$today->month,$today->year);
@@ -110,15 +104,9 @@ class ManageChartsController extends Controller
         // $site_activated_list = []; 
         $events_list = []; 
         foreach ($events as $key => $value) {
-            echo '<br> '; 
-            echo $value->_id; 
-            echo '<br> '; 
-            
+
             $event_date = $value->created_at->format('d/m/Y'); 
-            echo $event_date;             
-
             array_push($events_list,$event_date); 
-
 
             // print_r($value); 
             // $sitedata = Sites::find($value->site_id);
@@ -131,36 +119,25 @@ class ManageChartsController extends Controller
             // }
         }
 
-        print_r($days); 
-        echo '<hr> event list  : <br>  '; 
-        print_r($events_list);
-        echo '<hr> count event days  : <br>  '; 
+        // print_r($days); 
+        // echo '<hr> event list  : <br>  '; 
+        // print_r($events_list);
+        // echo '<hr> count event days  : <br>  '; 
         // $event_data_final = print_r(array_count_values($events_list)); 
         $event_data_final =array_count_values($events_list); 
 
-
         $now = Carbon::now();
-
         $dates = [$now->format('d/m/Y')];
-
         for($i = 1; $i < 31; $i++) {
           $dates[] = $now->subDays(1)->format('d/m/Y');
         }
+        // echo '<hr> all dates  : <br>  '; 
+        // print_r($dates);
 
 
-
-
-        echo '<hr> all dates  : <br>  '; 
-        print_r($dates);
-
-
-        echo '<hr> Final ARY  dates  : <br>  '; 
+        // echo '<hr> Final ARY  dates  : <br>  '; 
         $event_data_final1 = []; 
         foreach ($dates as $date_key => $date_value) {
-            echo '<br> '; 
-            // echo $date_value; 
-            // print_r($event_data_final); 
- 
             if (!array_key_exists($date_value,$event_data_final)){
                 $event_data_final1[$date_value] = 0; 
             }else{
@@ -170,10 +147,10 @@ class ManageChartsController extends Controller
 
 
         // use App\Models\Sites;
-        echo '<hr> '; 
-        print_r($event_data_final1); 
+        // echo '<hr> '; 
+        // print_r($event_data_final1); 
 
-        exit; 
+        // exit; 
         return view('charts.user-chart')->with('days',json_encode($dates,JSON_NUMERIC_CHECK))->with('event_data_final',json_encode($event_data_final,JSON_NUMERIC_CHECK));
     }
 
